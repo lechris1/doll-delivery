d15fa8906150706cbb8d34941f516fac19dcf96b
Chris Le <lechris1@msu.edu>
2019-04-04 20:02:12 -0400

    implementation of dijkstra's algorithm

---
+++ dijkstras_algorithm/src/Graph.kt
@@ -0,0 +1,112 @@
+class Graph(edges: List<Map<String, Any>>) {
+
+    val locationMap : HashMap<String, Int> = hashMapOf() // map locations to numbers
+    val idMap : HashMap<Int, String> = hashMapOf() // map ids to locations
+    var vertexIndex = -1
+    val numOfVertices = countVertices(edges)
+    val matrix = Array(numOfVertices*numOfVertices) {i -> 0} // 1D matrix
+
+    // Initialize graph as adjacency matrix
+    init {
+        var row : Int
+        var col : Int
+        // Insert edges into adjacency matrix
+        for (edge in edges) {
+            row = locationMap[edge["startLocation"]] as Int
+            col = locationMap[edge["endLocation"]] as Int
+
+            // Insert edge into matrix
+            matrix[getLocation(row, col)] = edge["distance"] as Int
+            matrix[getLocation(col, row)] = edge["distance"] as Int
+        }
+    }
+
+    fun dijkstra(startLocation: String, endLocation: String) : Map<String, Any> {
+        if (numOfVertices == 0) {
+            return mapOf<String, Any>("distance" to 0, "path" to "")
+        }
+
+        var start = locationMap[startLocation] as Int
+        var end = locationMap[endLocation]
+        var path = arrayListOf<Int>(*(Array(numOfVertices) {i -> -1})) // Shortest path
+        var availableNodes = arrayListOf<Int>(*(Array(numOfVertices) {i -> i})) // Available nodes
+        var distances = Array(numOfVertices) {i -> Int.MAX_VALUE} // Distances from starting node
+        distances[start] = 0
+
+        // Start searching
+        var nextShortest: Int
+        while (availableNodes.isNotEmpty()) {
+            nextShortest = findNextShortest(distances, availableNodes)
+
+            if (nextShortest == end) {
+                var pathString = getPath(path, distances, end)
+                return mapOf<String, Any>("distance" to distances[end], "path" to pathString.substring(0, pathString.length-4))
+            }
+
+            availableNodes.remove(nextShortest)
+
+            // Check if nextShortest is a dead end
+            for (j in 0 until numOfVertices) {
+                if (matrix[getLocation(nextShortest, j)] != 0 && j in availableNodes) {
+                    // Check that this edge leads to a shorter path
+                    if (matrix[getLocation(nextShortest, j)] + distances[nextShortest] < distances[j] ) {
+                        distances[j] = matrix[getLocation(nextShortest, j)] + distances[nextShortest]
+                        path[j] = nextShortest
+                    }
+                }
+            }
+        }
+
+        return mapOf<String, Any>("distance" to 0, "path" to "")
+    }
+
+    fun findNextShortest(distances: Array<Int>, availableNodes: ArrayList<Int>) : Int {
+        var min = Int.MAX_VALUE
+        var next = -1
+
+        // Find shortest distance node that can be on the path
+        for (i in 0 until numOfVertices) {
+            if (distances[i] < min && i in availableNodes) {
+                min = distances[i]
+                next = i
+            }
+        }
+        return next
+    }
+
+    fun getLocation(row: Int, col: Int) : Int = row * numOfVertices + col
+
+    fun printAdjacencyMatrix() {
+        for (i in 0 until numOfVertices) {
+            var result : String = ""
+            for (j in 0 until numOfVertices) {
+                result += String.format("%" + "3" + "s", matrix[getLocation(i, j)].toString())
+            }
+            println(result)
+        }
+    }
+
+    fun countVertices(edges: List<Map<String, Any>>) : Int {
+        for (edge in edges) {
+            // Map location string to integer
+            if (edge["startLocation"] !in locationMap) {
+                locationMap[edge["startLocation"] as String] = ++vertexIndex
+                idMap[vertexIndex] = edge["startLocation"] as String
+            }
+            if (edge["endLocation"] !in locationMap) {
+                locationMap[edge["endLocation"] as String] = ++vertexIndex
+                idMap[vertexIndex] = edge["endLocation"] as String
+            }
+        }
+        return vertexIndex+1
+    }
+
+    fun getPath(path: ArrayList<Int>, distances: Array<Int>, end: Int) : String {
+        if (path[end] == -1) {
+            return idMap[end] + " => "
+        }
+
+        return getPath(path, distances, path[end]) + idMap[end] + " => "
+    }
+
+}
---
+++ dijkstras_algorithm/src/GraphTest.kt
@@ -0,0 +1,142 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Disabled
+
+internal class GraphTest {
+    fun dijkstra(startLocation: String, endLocation: String, edges: List<Map<String, Any>>): Map<String, Any> {
+        var graph = Graph(edges)
+        return graph.dijkstra(startLocation, endLocation)
+    }
+
+    @Test
+    fun givenTest() {
+        var actual = dijkstra(
+            startLocation = "Kruthika's abode",
+            endLocation = "Craig's haunt",
+            edges =
+            listOf(
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Mark's crib", "distance" to 9),
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Greg's casa", "distance" to 4),
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Matt's pad", "distance" to 18),
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Brian's apartment", "distance" to 8),
+                mapOf("startLocation" to "Brian's apartment", "endLocation" to "Wesley's condo", "distance" to 7),
+                mapOf("startLocation" to "Brian's apartment", "endLocation" to "Cam's dwelling", "distance" to 17),
+                mapOf("startLocation" to "Greg's casa", "endLocation" to "Cam's dwelling", "distance" to 13),
+                mapOf("startLocation" to "Greg's casa", "endLocation" to "Mike's digs", "distance" to 19),
+                mapOf("startLocation" to "Greg's casa", "endLocation" to "Matt's pad", "distance" to 14),
+                mapOf("startLocation" to "Wesley's condo", "endLocation" to "Kirk's farm", "distance" to 10),
+                mapOf("startLocation" to "Wesley's condo", "endLocation" to "Nathan's flat", "distance" to 11),
+                mapOf("startLocation" to "Wesley's condo", "endLocation" to "Bryce's den", "distance" to 6),
+                mapOf("startLocation" to "Matt's pad", "endLocation" to "Mark's crib", "distance" to 19),
+                mapOf("startLocation" to "Matt's pad", "endLocation" to "Nathan's flat", "distance" to 15),
+                mapOf("startLocation" to "Matt's pad", "endLocation" to "Craig's haunt", "distance" to 14),
+                mapOf("startLocation" to "Mark's crib", "endLocation" to "Kirk's farm", "distance" to 9),
+                mapOf("startLocation" to "Mark's crib", "endLocation" to "Nathan's flat", "distance" to 12),
+                mapOf("startLocation" to "Bryce's den", "endLocation" to "Craig's haunt", "distance" to 10),
+                mapOf("startLocation" to "Bryce's den", "endLocation" to "Mike's digs", "distance" to 9),
+                mapOf("startLocation" to "Mike's digs", "endLocation" to "Cam's dwelling", "distance" to 20),
+                mapOf("startLocation" to "Mike's digs", "endLocation" to "Nathan's flat", "distance" to 12),
+                mapOf("startLocation" to "Cam's dwelling", "endLocation" to "Craig's haunt", "distance" to 18),
+                mapOf("startLocation" to "Nathan's flat", "endLocation" to "Kirk's farm", "distance" to 3)
+            ))
+
+        var expected = mapOf("distance" to 31, "path" to "Kruthika's abode => Brian's apartment => Wesley's condo => Bryce's den => Craig's haunt")
+
+        assertEquals(expected, actual, "Failed Test 1")
+    }
+
+    @Test
+    fun G4GTests() {
+        val edges = listOf(
+            mapOf("startLocation" to "0", "endLocation" to "1", "distance" to 4),
+            mapOf("startLocation" to "0", "endLocation" to "7", "distance" to 8),
+            mapOf("startLocation" to "1", "endLocation" to "2", "distance" to 8),
+            mapOf("startLocation" to "1", "endLocation" to "7", "distance" to 11),
+            mapOf("startLocation" to "2", "endLocation" to "3", "distance" to 7),
+            mapOf("startLocation" to "2", "endLocation" to "5", "distance" to 4),
+            mapOf("startLocation" to "2", "endLocation" to "8", "distance" to 2),
+            mapOf("startLocation" to "3", "endLocation" to "4", "distance" to 9),
+            mapOf("startLocation" to "3", "endLocation" to "5", "distance" to 14),
+            mapOf("startLocation" to "4", "endLocation" to "5", "distance" to 10),
+            mapOf("startLocation" to "5", "endLocation" to "6", "distance" to 2),
+            mapOf("startLocation" to "6", "endLocation" to "7", "distance" to 1),
+            mapOf("startLocation" to "6", "endLocation" to "8", "distance" to 6),
+            mapOf("startLocation" to "7", "endLocation" to "8", "distance" to 7)
+        )
+
+        var actual = dijkstra(
+            startLocation = "0",
+            endLocation = "8",
+            edges = edges
+            )
+
+        var expected = mapOf("distance" to 14, "path" to "0 => 1 => 2 => 8")
+        assertEquals(expected, actual, "Failed Test 0-8")
+
+        actual = dijkstra(
+            startLocation = "0",
+            endLocation = "4",
+            edges = edges
+        )
+
+        expected = mapOf("distance" to 21, "path" to "0 => 7 => 6 => 5 => 4")
+        assertEquals(expected, actual, "Failed Test 0-4")
+
+        actual = dijkstra(
+            startLocation = "0",
+            endLocation = "1",
+            edges = edges
+        )
+
+        expected = mapOf("distance" to 4, "path" to "0 => 1")
+        assertEquals(expected, actual, "Failed Test 0-1")
+    }
+
+    @Test
+    fun emptyTest() {
+        var actual = dijkstra(
+            startLocation = "0",
+            endLocation = "0",
+            edges = listOf())
+
+        var expected = mapOf("distance" to 0, "path" to "")
+        assertEquals(expected, actual, "Result not empty")
+    }
+
+    @Test
+    fun negativeEdgeTest() {
+        var actual = dijkstra(
+            startLocation = "Kruthika's abode",
+            endLocation = "Craig's haunt",
+            edges =
+            listOf(
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Mark's crib", "distance" to 9),
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Greg's casa", "distance" to 4),
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Matt's pad", "distance" to 18),
+                mapOf("startLocation" to "Kruthika's abode", "endLocation" to "Brian's apartment", "distance" to 8),
+                mapOf("startLocation" to "Brian's apartment", "endLocation" to "Wesley's condo", "distance" to 7),
+                mapOf("startLocation" to "Brian's apartment", "endLocation" to "Cam's dwelling", "distance" to 17),
+                mapOf("startLocation" to "Greg's casa", "endLocation" to "Cam's dwelling", "distance" to 13),
+                mapOf("startLocation" to "Greg's casa", "endLocation" to "Mike's digs", "distance" to 19),
+                mapOf("startLocation" to "Greg's casa", "endLocation" to "Matt's pad", "distance" to 14),
+                mapOf("startLocation" to "Wesley's condo", "endLocation" to "Kirk's farm", "distance" to 10),
+                mapOf("startLocation" to "Wesley's condo", "endLocation" to "Nathan's flat", "distance" to 11),
+                mapOf("startLocation" to "Wesley's condo", "endLocation" to "Bryce's den", "distance" to 6),
+                mapOf("startLocation" to "Matt's pad", "endLocation" to "Mark's crib", "distance" to 19),
+                mapOf("startLocation" to "Matt's pad", "endLocation" to "Nathan's flat", "distance" to 15),
+                mapOf("startLocation" to "Matt's pad", "endLocation" to "Craig's haunt", "distance" to 14),
+                mapOf("startLocation" to "Mark's crib", "endLocation" to "Kirk's farm", "distance" to 9),
+                mapOf("startLocation" to "Mark's crib", "endLocation" to "Nathan's flat", "distance" to 12),
+                mapOf("startLocation" to "Bryce's den", "endLocation" to "Craig's haunt", "distance" to 10),
+                mapOf("startLocation" to "Bryce's den", "endLocation" to "Mike's digs", "distance" to 9),
+                mapOf("startLocation" to "Mike's digs", "endLocation" to "Cam's dwelling", "distance" to 20),
+                mapOf("startLocation" to "Mike's digs", "endLocation" to "Nathan's flat", "distance" to 12),
+                mapOf("startLocation" to "Cam's dwelling", "endLocation" to "Craig's haunt", "distance" to 18),
+                mapOf("startLocation" to "Nathan's flat", "endLocation" to "Kirk's farm", "distance" to 3)
+            ))
+
+        var expected = mapOf("distance" to 0, "path" to "")
+
+        assertEquals(expected, actual, "Negative Edges Not Allowed")
+    }
+}
